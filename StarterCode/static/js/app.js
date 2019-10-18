228ce6f7851f731a56cbb99076e4bb3301eb5db4
Luis Maldonado <lmaldonado93@gmail.com>
2019-10-17 22:24:15 -0400

    second_push

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,65 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var resetbtn = d3.select("#reset-btn");
+
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+var populate = (tableData) => {
+  tableData.forEach((datetime)=>{
+      var row = tbody.append("tr");
+      Object.entries(datetime).forEach(([key,value])=> {
+         var cell=row.append("td");
+          cell.text(value);
+      });
+  });
+};
+
+populate(tableData);
+
+var button = d3.select("#filter-btn");
+
+button.on("click",() => {
+
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value").trim();
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  tbody.html("");
+
+  let response = {
+    filteredData
+  }
+
+  if (response.filteredData.length !== 0) {
+    populate(filteredData);
+  }
+    else if (response.filteredData.length === 0){
+     populate(filteredData);
+  
+    }
+    else {
+      tbody.append("tr").append("td").text("No results found!"); 
+    }
+})
+
+resetbtn.on("click", () => {
+  tbody.html("");
+  populate(tableData);
+  console.log("Table reset");
+
+
+
+
+
+});
